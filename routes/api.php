1817e209e2b3c0d7bc7da049c0bc26ee10408c32
HungNP <21520252@gm.uit.edu.vn>
2023-11-18 15:42:52 +0700

    rename

--- routes/api.php
+++ routes/api.php
@@ -22,7 +22,7 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 });
 
 
-Route::post('/login', [AuthController::class, 'login']);
+Route::post('/signin', [AuthController::class, 'login']);
 
 Route::delete('seed/DB', function () {
     db::statement('DROP SCHEMA public CASCADE');
