59c13c203a7712378c0b0a031ca733da1b5c5909
HungNP <21520252@gm.uit.edu.vn>
2023-11-28 13:57:22 +0700

    remove lib

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -7,7 +7,6 @@ use App\Models\Account;
 use Firebase\JWT\JWT;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Validator;
-use Tymon\JWTAuth\Facades\JWTAuth;
 
 class AuthController extends Controller
 {
